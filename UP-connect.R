634daddd680bfdfdfc746b5a82f84ad5fcc0c7c1
Naxin (Nancy) Wang <nancywnsun@gmail.com>
2014-03-19 00:25:46 -0700

    new r file

--- UP-connect.R
+++ UP-connect.R
@@ -3,8 +3,6 @@
 
 library(RCurl)
 library(RJSONIO)
-##take in sample account information
-#source("accountinfo.R")
 
 jawboneActivities <- setRefClass(Class = "jawboneActivities",
                                 fields = list(username = "character",
@@ -17,19 +15,19 @@ jawboneActivities <- setRefClass(Class = "jawboneActivities",
                                 ))
 
 jawboneActivities$methods(
-  initialize = function(...) {
+  initialize = function(username,password) {
+    username <<- username
+    password <<- password
     sleep <<- vector("list")
     move <<- vector("list")
     workout <<- vector("list")
     goal <<- vector("list")
     access_token <<- character()
-    callSuper(...)
   })
 
 jawboneActivities$methods(
   connectJU = function() {
-    "Login to Jawbone Connect"
-    
+    #"Login to Jawbone Connect"
     #Connect to Jawbone UP API via postForm
     
     connectUP.j = postForm(uri = "https://jawbone.com/user/signin/login", .params=list(email=username, pwd = password, service = "nudge"),.checkParams = TRUE)
@@ -50,11 +48,11 @@ jawboneActivities$methods(
     if(is.na(access_token)) stop("Not connected to Jawebone UP. Please call function connectJU")
     else{
       
-    sleep.j<-getURL(url=paste("https://jawbone.com/nudge/api/users/@me/sleeps?limit=0"),httpheader=c("x-nudge-token"=access_token)) #,verbose=TRUE
+    sleep.j<-getURL(url=paste("https://jawbone.com/nudge/api/users/@me/sleeps?limit=0"),httpheader=c("x-nudge-token"=access_token))
     sleep.parsed <- fromJSON(sleep.j)
 
-    #names(sleep.parsed$data$items[[1]])
     s_size<-1:length(sleep.parsed$data$items) #number of all the sleep activities recorded
+
     #******extract key sleep data******
     #label explanation source: https://jawbone.com/up/developer/endpoints/sleeps
 
@@ -69,59 +67,43 @@ jawboneActivities$methods(
     s_quality <- unlist(lapply(s_size, function(i) sleep.parsed$data$items[[i]]$details$quality)) #Sleep quality for the night. Based on a JAWBONE proprietary formula of light and deep sleep vs wake time.
     s_rem <- unlist(lapply(s_size, function(i) sleep.parsed$data$items[[i]]$details$rem)) #rem is not in used in Jawbone UP yet
 
-    str(s_quality)
-    str(s_duration)
-    print(s_quality)
-    
     sleep <<- list("s_xid"=s_xid,"s_date"=s_date,"s_subtype"=s_subtype,"s_duration"=s_duration,
                   "s_asleep_time"=s_asleep_time,"s_awake"=s_awake,"s_deep"=s_deep,"s_light"=s_light,"s_quality"=s_quality,"s_rem"=s_rem)
-    
-    #return(sleep)
     }
 },
 
   retrieveJUmoveList = function() {
     
     #obtain move data
-    move.j<-getURL(url=paste("https://jawbone.com/nudge/api/users/@me/moves?limit=0"),httpheader=c("x-nudge-token"=access_token),verbose=TRUE)
+    move.j<-getURL(url=paste("https://jawbone.com/nudge/api/users/@me/moves?limit=0"),httpheader=c("x-nudge-token"=access_token))
     move.parsed <- fromJSON(move.j)
-    
     m_size <- 1:length(move.parsed$data$items)
-    #move$data$size
-    #names(move$data$items[[1]]$details)
     
     #******extract key activity data******
     #label explanation source: https://jawbone.com/up/developer/endpoints/moves
     
     m_xid <- unlist(lapply(m_size, function(i) move.parsed$data$items[[i]]$xid)) #unique id of this specific event
     m_date <- unlist(lapply(m_size, function(i) move.parsed$data$items[[i]]$date)) #Date when this move was created factoring in user timezone, formatted as YYYYMMDD
-    m_distance <- unlist(lapply(m_size, function(i) move.parsed$data$items[[i]]$detail$distance))
-    m_distance_km <- unlist(lapply(m_size, function(i) move.parsed$data$items[[i]]$detail$km))
-    m_inactive_time <- unlist(lapply(m_size, function(i) move.parsed$data$items[[i]]$detail$inactive_time))
-    m_longest_active <- unlist(lapply(m_size, function(i) move.parsed$data$items[[i]]$detail$longest_active))
-    m_longest_idle <- unlist(lapply(m_size, function(i) move.parsed$data$items[[i]]$detail$longest_idle))
-    m_steps <- unlist(lapply(m_size, function(i) move.parsed$data$items[[i]]$detail$steps))
-    m_calories <- unlist(lapply(m_size, function(i) move.parsed$data$items[[i]]$detail$calories))
-    m_workout_count <- unlist(lapply(m_size, function(i) move.parsed$data$items[[i]]$detail$wo_count))
-    m_workout_time <- unlist(lapply(m_size, function(i) move.parsed$data$items[[i]]$detail$wo_time))
+    m_distance_km <- unlist(lapply(m_size, function(i) move.parsed$data$items[[i]]$detail$km)) #Distance travelled, in kilometers
+    m_longest_active <- unlist(lapply(m_size, function(i) move.parsed$data$items[[i]]$detail$longest_active)) #Longest consecutive active period, in seconds
+    m_longest_idle <- unlist(lapply(m_size, function(i) move.parsed$data$items[[i]]$detail$longest_idle)) #Longest consecutive inactive period, in seconds
+    m_steps <- unlist(lapply(m_size, function(i) move.parsed$data$items[[i]]$detail$steps)) #Number of steps taken
+    m_calories <- unlist(lapply(m_size, function(i) move.parsed$data$items[[i]]$detail$calories)) #Total calories burned
+    m_workout_count <- unlist(lapply(m_size, function(i) move.parsed$data$items[[i]]$detail$wo_count)) #Number of workouts logged during this move
+    m_workout_time <- unlist(lapply(m_size, function(i) move.parsed$data$items[[i]]$detail$wo_time)) #Total time spent in workouts, in seconds
     
-    move <<- list("m_xid"=m_xid,"m_date"=m_date,"m_distance"=m_distance,
-                  "m_distance_km"=m_distance_km,"m_inactive_time"=m_inactive_time,"m_longest_active"=m_longest_active,
-                  "m_longest_idle"=m_longest_idle,"m_steps"=m_steps,"m_calories"=m_calories,"m_workout_count"=m_workout_count,
-                  "m_workout_time"=m_workout_time)
-    
-    print(m_calories)
+    move <<- list("m_xid"=m_xid,"m_date"=m_date,"m_distance_km"=m_distance_km,"m_longest_active"=m_longest_active,
+                  "m_longest_idle"=m_longest_idle,"m_steps"=m_steps,"m_calories"=m_calories,
+                  "m_workout_count"=m_workout_count, "m_workout_time"=m_workout_time)
   },
 
     retrieveJUworkoutList = function() {
       
       #obtain workout data
-      workout.j<-getURL(url=paste("https://jawbone.com/nudge/api/users/@me/workouts?limit=0"),httpheader=c("x-nudge-token"=access_token),verbose=TRUE)
+      workout.j<-getURL(url=paste("https://jawbone.com/nudge/api/users/@me/workouts?limit=0"),httpheader=c("x-nudge-token"=access_token))
       workout.parsed <- fromJSON(workout.j)
       
       wo_size <- 1:length(workout.parsed$data$items)
-      names(workout.parsed$data$items[[1]]$details)
-      names(workout.parsed$data$item[[1]])
       
       #******extract key activity data******
       #label explanation source: https://jawbone.com/up/developer/endpoints/moves
@@ -143,12 +125,9 @@ jawboneActivities$methods(
 
   retriveJUgoalList = function() {
     #obtain goal data
-    goal.j<-getURL(url=paste("https://jawbone.com/nudge/api/users/@me/goals"),httpheader=c("x-nudge-token"=access_token),verbose=TRUE)
+    goal.j<-getURL(url=paste("https://jawbone.com/nudge/api/users/@me/goals"),httpheader=c("x-nudge-token"=access_token))
     goal.parsed <- fromJSON(goal.j)
     
-    names(goal$data)
-    
-    #extract goal data
     sleep_goal <- goal.parsed$data$sleep_total
     step_goal <- goal.parsed$data$move_steps
     
@@ -162,13 +141,12 @@ jawboneActivities$methods(
     s_endTime <- as.POSIXct(s_endTime)
     cat("The sleep starts at",format(s_startTime), "and ends at",format(s_endTime),"\n")
     createSleep.j <- postForm(uri = "https://jawbone.com/nudge/api/users/@me/sleeps",
-                            .opts=list(httpheader=c("x-nudge-token"= access_token)), #,verbose=TRUE)
+                            .opts=list(httpheader=c("x-nudge-token"= access_token)),
                             .params=list(time_created=as.integer(s_startTime),
                                          time_completed=as.integer(s_endTime)),
                             style="POST", .checkParams = TRUE)
     createSleep.parsed <- fromJSON(createSleep.j)
-    print(createSleep.parsed$meta$message == "Created")
-    print(createSleep.parsed)
+    print(createSleep.parsed$meta$message)
     if(createSleep.parsed$meta$message == "Created"){
       cat("The sleep event",createSleep.parsed$data$xid, "(", 
           format(as.POSIXct(createSleep.parsed$data$details$awake_time,origin="1970-01-01")),
@@ -180,9 +158,9 @@ jawboneActivities$methods(
       return(print("Failed to create a sleep event. Please check the input format or try reconnect to Jawbone UP."))
   },
 
-deleteJUsleep= function(s_xid="character"){
+  deleteJUsleep= function(s_xid="character"){
     deleteSleep.j <- httpDELETE(url = paste("https://jawbone.com/nudge/api/sleeps/",s_xid,sep=""), 
-                                httpheader=c("x-nudge-token"=access_token),verbose=TRUE)
+                                httpheader=c("x-nudge-token"=access_token)) #,verbose=TRUE)
     deleteSleep.parsed <- fromJSON(deleteSleep.j)
     print(deleteSleep.parsed$meta$message)
     if(deleteSleep.parsed$meta$message=="OK"){
@@ -193,14 +171,12 @@ deleteJUsleep= function(s_xid="character"){
         print("Failed to delete a sleep event. Please check the input format or try reconnect to Jawbone UP.")
     },
 
-createJUworkout = function(wo_startTime="character",wo_endTime="character",wo_subType="numeric",
+  createJUworkout = function(wo_startTime="character",wo_endTime="character",wo_subType="numeric",
                            wo_calories="numeric",wo_distance="numeric",wo_intensity="integer"){
   #required parameters: startTime, endTime, and intensity
   #timezone is the local computer timezone
   #time format:"2014-3-15 12:00:00"
-  print(wo_startTime)
   wo_startTime<-as.POSIXct(wo_startTime)
-  print(wo_startTime)
   wo_endTime <- as.POSIXct(wo_endTime)
   cat("The workout starts at",format(wo_startTime), "and ends at",format(wo_endTime),"\n")
   if(missing(wo_calories)) wo_calories <- 0
@@ -210,7 +186,7 @@ createJUworkout = function(wo_startTime="character",wo_endTime="character",wo_su
                             .opts=list(httpheader=c("x-nudge-token"= access_token)), #, verbose=TRUE
                             .params=list(time_created=as.integer(wo_startTime),
                                          time_completed=as.integer(wo_endTime),
-                                         subtype=wo_subType,
+                                         sub_type=wo_subType,
                                          calories=wo_calories,
                                          distance=wo_distance,
                                          intensity=wo_intensity),
@@ -229,7 +205,7 @@ createJUworkout = function(wo_startTime="character",wo_endTime="character",wo_su
     return(print("Failed to create a workout event. Please check the input format or try reconnect to Jawbone UP."))
 },
 
-deleteJUworkout= function(wo_xid="character"){
+  deleteJUworkout= function(wo_xid="character"){
   deleteWorkout.j <- httpDELETE(url = paste("https://jawbone.com/nudge/api/workouts/",wo_xid,sep=""), 
                               httpheader=c("x-nudge-token"=access_token),verbose=TRUE)
   deleteWorkout.parsed <- fromJSON(deleteWorkout.j)
@@ -242,50 +218,58 @@ deleteJUworkout= function(wo_xid="character"){
     print("Failed to delete a workout event. Please check the input format or try reconnect to Jawbone UP.")
 },
 
-sleepQualvsQuanGraph = function(){
+  sleepQualvsQuanGraph = function(){
   .self$retrieveJUsleepList()
   sleep.df <- as.data.frame(.self$sleep, stringsAsFactors=FALSE)
   sleep.df <- sleep.df[!sleep.df$s_quality==0,]
-  print(sleep.df)
   sleep.qualquan <- ggplot(sleep.df,aes(x=s_duration/3600,y=s_quality))+geom_point()+
       labs(x = "Hours of sleep", y = "Sleep quality")
-  sleep.qualquan + facet_wrap(~s_subtype, scales = "free_y")
+  sleep.qualquan
 },
 
-sleepDurationGraph = function(){
+  sleepDurationGraph = function(){
   .self$retrieveJUsleepList()
   sleep.df <- as.data.frame(.self$sleep, stringsAsFactors=FALSE)
+  sleep.df$s_subtype[sleep.df$s_subtype == 0] <- "Normal sleep"
+  sleep.df$s_subtype[sleep.df$s_subtype == 1] <- "Power nap"
+  sleep.df$s_subtype[sleep.df$s_subtype == 2] <- "Other nap"
   date.str<-unlist(lapply(sleep.df$s_date,as.character))
   date.str<-as.Date(date.str,"%Y%m%d")
   sleep.df <- data.frame(sleep.df,date.str)
-  sleep.duration <- ggplot(sleep.df, aes(x=date.str, y=s_duration/3600))+geom_point()+
+  sleep.duration <- ggplot(sleep.df, aes(x=date.str, y=s_duration/3600))+geom_line()+
     labs(x = "Date", y = "Hours of sleep") + scale_x_date(labels = date_format("%m-%Y"))
-  sleep.duration + facet_wrap(~s_subtype, scales = "free_y")
+  sleep.duration + facet_wrap(~s_subtype)
 },
 
 sleepQualityGraph = function(){
   .self$retrieveJUsleepList()
   sleep.df <- as.data.frame(.self$sleep, stringsAsFactors=FALSE)
+  sleep.df <- sleep.df[!sleep.df$s_quality==0,]
+  sleep.df$s_subtype[sleep.df$s_subtype == 0] <- "Normal sleep"
+  sleep.df$s_subtype[sleep.df$s_subtype == 1] <- "Power nap"
+  sleep.df$s_subtype[sleep.df$s_subtype == 2] <- "Other nap"
   date.str<-unlist(lapply(sleep.df$s_date,as.character))
   date.str<-as.Date(date.str,"%Y%m%d")
   sleep.df <- data.frame(sleep.df,date.str)
-  sleep.quality <- ggplot(sleep.df, aes(x=date.str, y=s_quality))+geom_point()+
+  sleep.quality <- ggplot(sleep.df, aes(x=date.str, y=s_quality))+geom_line()+
     labs(x = "Date", y = "Sleep quality") +
     scale_x_date(labels = date_format("%m-%Y"))
-  sleep.quality + facet_wrap(~s_subtype, scales = "free_y")
+  sleep.quality + facet_wrap(~s_subtype)
 },
 
 moveStepGraph = function(){
   .self$retrieveJUmoveList()
   move.df <- as.data.frame(.self$move, stringsAsFactors=FALSE)
-  date.str<-unlist(lapply(sleep.df$s_date,as.character))
+  move.df <- move.df[!move.df$m_steps==0,]
+  date.str<-unlist(lapply(move.df$m_date,as.character))
   date.str<-as.Date(date.str,"%Y%m%d")
   move.df <- data.frame(move.df,date.str)
-  move.step <- ggplot(move.df, aes(x=date.str, y=m_steps))+geom_line()+  #make it a line trend
+  move.step <- ggplot(move.df, aes(x=date.str, y=m_steps))+geom_line()+  
     labs(x = "Date", y = "Steps taken") +
     scale_x_date(labels = date_format("%m-%Y"))
-  
-  #add calorie line; add a few more lines
+  move.step
 }
 )
+
+package.skeleton(name="JawboneUPConnect", code_files="UP-connect.R")
 
